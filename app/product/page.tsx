eb63fad1fcdbfbe46ce9adfdf228440440c5f33f
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-09-18 10:49:06 +0200

    structure

---
+++ app/product/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import { addProduct, getProduct } from "@/actions/action";
+import Link from 'next/link';
+
+export default function Product() {
+ 
+
+  return (
+    <>
+      
+    </>
+  );
+}
