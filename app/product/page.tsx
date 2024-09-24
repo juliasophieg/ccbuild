1435f13040d252c11b12231d3413453aaab9e62a
Julia <jsgoransson@gmail.com>
2024-09-24 15:53:39 +0200

    projectId prop is needed

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,14 +1,12 @@
-
 import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
-
- 
+  const projectId = "12345"; // Ändra till rätt id!!
 
   return (
     <>
-    <div className="text-red-400">
-        <ProductForm />
+      <div className="text-red-400">
+        <ProductForm projectId={projectId} />
       </div>
     </>
   );
