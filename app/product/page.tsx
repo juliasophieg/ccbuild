293c023f9133c80446ab07bde4ab5807cedab66d
Julia <142974149+juliasophieg@users.noreply.github.com>
2024-09-24 17:16:10 +0200

    changed back

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,12 +1,11 @@
 import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
-  const projectId = "12345"; // Ändra till rätt id!!
 
   return (
     <>
       <div className="text-red-400">
-        <ProductForm projectId={projectId} />
+        <ProductForm/>
       </div>
     </>
   );
