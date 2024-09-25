6cbca0d51b1dee2d3d5c058f1fe3caf7236b6dd4
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-09-25 10:28:08 +0200

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,91 +1,85 @@
-import { FC } from "react";
-import Link from "next/link";
-import { Button } from "@mui/material";
-import { ProductFormData } from "@/schemas";
+import { FC } from 'react'
+import Link from 'next/link'
+import { Button } from '@mui/material'
+import { ProductFormData } from '@/schemas'
 
 type PageProps = {
   params: {
-    slug: string;
-  };
-};
+    slug: string
+  }
+}
 
 type ProjectData = {
-  _id: string;
-  name: string;
-  date: string;
-  description: string;
-};
+  _id: string
+  name: string
+  date: string
+  description: string
+}
 
 const Page: FC<PageProps> = async ({ params }) => {
-  let project: ProjectData | null = null;
-  let products: ProductFormData[] = [];
-  let errorMessage = "";
+  let project: ProjectData | null = null
+  let products: ProductFormData[] = []
+  let errorMessage = ''
 
   // Fetch project data based on slug
   try {
-    const res = await fetch(
-      `http://localhost:3000/api/projects/${params.slug}`,
-      {
-        cache: "no-store",
-      }
-    );
+    const res = await fetch(`/api/projects/${params.slug}`, {
+      cache: 'no-store',
+    })
 
     if (!res.ok) {
-      throw new Error("Failed to fetch project data");
+      throw new Error('Failed to fetch project data')
     }
 
-    project = await res.json();
+    project = await res.json()
   } catch (error) {
     errorMessage =
-      error instanceof Error ? error.message : "Unknown error occurred";
+      error instanceof Error ? error.message : 'Unknown error occurred'
   }
 
   if (!project) {
-    return <p>{errorMessage || "Project not found"}</p>;
+    return <p>{errorMessage || 'Project not found'}</p>
   }
 
   // Fetch products related to the projectId (project._id)
   try {
-    const resProducts = await fetch(
-      `http://localhost:3000/api/products/${project._id}`,
-      {
-        cache: "no-store",
-      }
-    );
+    const resProducts = await fetch(`/api/products/${project._id}`, {
+      cache: 'no-store',
+    })
 
     if (!resProducts.ok) {
-      throw new Error("Failed to fetch products");
+      throw new Error('Failed to fetch products')
     }
-    console.log(resProducts);
-    products = await resProducts.json();
+    console.log(resProducts)
+    products = await resProducts.json()
   } catch (error) {
     errorMessage =
       error instanceof Error
         ? error.message
-        : "Unknown error occurred while fetching products";
+        : 'Unknown error occurred while fetching products'
   }
 
   return (
     <>
-      <div className="mb-8">
-        <h1 className="text-4xl font-bold">{project.name}</h1>
-        <p className="mt-2 text-lg">Date: {project.date}</p>
-        <p className="mt-2">{project.description}</p>
+      <div className='mb-8'>
+        <h1 className='text-4xl font-bold'>{project.name}</h1>
+        <p className='mt-2 text-lg'>Date: {project.date}</p>
+        <p className='mt-2'>{project.description}</p>
       </div>
 
       <div>
-        <h2 className="mb-6 text-3xl font-semibold">
+        <h2 className='mb-6 text-3xl font-semibold'>
           Products for this Project
         </h2>
         {products.length > 0 ? (
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {products.map((product) => (
+          <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+            {products.map(product => (
               <div
                 key={product._id}
-                className="flex aspect-square flex-col justify-between rounded-lg bg-white p-6 text-black shadow-lg"
+                className='flex aspect-square flex-col justify-between rounded-lg bg-white p-6 text-black shadow-lg'
               >
                 {product?.generalInformation?.productName ||
-                  "Product Name Not Available"}
+                  'Product Name Not Available'}
               </div>
             ))}
           </div>
@@ -95,12 +89,12 @@ const Page: FC<PageProps> = async ({ params }) => {
       </div>
 
       <Link href={`/projects/${params.slug}/product/create`}>
-        <Button variant="contained" color="primary" className="mt-8">
+        <Button variant='contained' color='primary' className='mt-8'>
           Create Product
         </Button>
       </Link>
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
