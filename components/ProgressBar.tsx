9d5f24ac0d878af08e4ede29251f1692ace6030a
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-09-25 20:49:33 +0200

    Update ProgressBar.tsx

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -5,15 +5,41 @@ import { useCategoryContext } from '@/context/CategoryContext'
 
 const ProgressBar: React.FC = () => {
   const { selectedStep } = useCategoryContext()
+
+  const steps = [
+    'Generell information',
+    'Egenskaper',
+    'Marknadsplats',
+    'Plats/Status/Antal',
+    'Överblick',
+  ]
+
+  const currentStepIndex = steps.indexOf(selectedStep)
+
   return (
-    <div className='flex grow basis-1/5 flex-col gap-4'>
-      <h1>{selectedStep}</h1>
-      <div>HEJ</div>
-      <div>HEJ</div>
-      <div>HEJ</div>
-      <div>HEJ</div>
-      <div>HEJ</div>
-      <div>HEJ</div>
+    <div className='flex flex-col gap-4'>
+      {steps.map((step, index) => (
+        <div key={index} className='flex items-center gap-2'>
+          <div
+            className={`flex h-6 w-6 items-center justify-center rounded-full ${
+              index <= currentStepIndex
+                ? 'bg-blue-500 text-white'
+                : 'bg-gray-300 text-gray-500'
+            }`}
+          >
+            {index + 1}
+          </div>
+          <div
+            className={`${
+              index <= currentStepIndex
+                ? 'font-bold text-black'
+                : 'text-gray-500'
+            }`}
+          >
+            {step}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
