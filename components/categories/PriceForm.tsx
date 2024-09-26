0878d89d99aa460d4b775b486daafb1aca9d0d92
Julia <jsgoransson@gmail.com>
2024-09-26 21:51:38 +0200

    step 3 styling

--- components/categories/PriceForm.tsx
+++ components/categories/PriceForm.tsx
@@ -1,63 +1,80 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useFormContext } from 'react-hook-form'
-import { ProductFormData } from '@/schemas'
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { ProductFormData } from "@/schemas";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 const PriceForm: React.FC = React.memo(() => {
   const {
     register,
     formState: { errors },
-  } = useFormContext<ProductFormData>()
+  } = useFormContext<ProductFormData>();
 
   return (
-    <fieldset>
-      <legend>Price</legend>
-
-      <div>
-        <label htmlFor='internalPrice'>Internal Price:</label>
-        <input
-          id='internalPrice'
-          type='number'
-          {...register('price.internalPrice', {
-            setValueAs: value => parseFloat(value),
+    <fieldset className="flex flex-col gap-4">
+      <h3 className="text-lg">Pris</h3>
+      {/* Internal Price */}
+      <div className="flex gap-4">
+        <TextField
+          className="w-2/6"
+          required
+          label="Internt pris/styck"
+          variant="outlined"
+          size="small"
+          type="number"
+          id="internalPrice"
+          {...register("price.internalPrice", {
+            setValueAs: (value) => parseFloat(value),
           })}
-          aria-invalid={errors.price?.internalPrice ? 'true' : 'false'}
+          aria-invalid={errors.price?.internalPrice ? "true" : "false"}
+          error={!!errors.price?.internalPrice}
+          helperText={errors.price?.internalPrice?.message}
+          slotProps={{
+            inputLabel: { shrink: true },
+          }}
         />
-        {errors.price?.internalPrice && (
-          <p className='error'>{errors.price.internalPrice.message}</p>
-        )}
-      </div>
 
-      <div>
-        <label htmlFor='externalPrice'>External Price:</label>
-        <input
-          id='externalPrice'
-          type='number'
-          {...register('price.externalPrice', {
-            setValueAs: value => parseFloat(value),
+        {/* External Price */}
+        <TextField
+          className="w-2/6"
+          required
+          label="Externt pris/styck"
+          variant="outlined"
+          size="small"
+          type="number"
+          id="externalPrice"
+          {...register("price.externalPrice", {
+            setValueAs: (value) => parseFloat(value),
           })}
-          aria-invalid={errors.price?.externalPrice ? 'true' : 'false'}
+          aria-invalid={errors.price?.externalPrice ? "true" : "false"}
+          error={!!errors.price?.externalPrice}
+          helperText={errors.price?.externalPrice?.message}
+          slotProps={{
+            inputLabel: { shrink: true },
+          }}
         />
-        {errors.price?.externalPrice && (
-          <p className='error'>{errors.price.externalPrice.message}</p>
-        )}
-      </div>
 
-      <div>
-        <label htmlFor='buyerPrice'>Buyer Price:</label>
-        <input
-          id='buyerPrice'
-          type='checkbox'
-          {...register('price.buyerPrice')}
-          aria-invalid={errors.price?.buyerPrice ? 'true' : 'false'}
+        {/* Buyer Price as Checkbox */}
+        <FormControlLabel
+          className="w-2/6"
+          control={
+            <Checkbox
+              id="buyerPrice"
+              {...register("price.buyerPrice")}
+              aria-invalid={errors.price?.buyerPrice ? "true" : "false"}
+            />
+          }
+          label="Låt köparen föreslå pris"
         />
         {errors.price?.buyerPrice && (
-          <p className='error'>{errors.price.buyerPrice.message}</p>
+          <p className="error">{errors.price.buyerPrice.message}</p>
         )}
       </div>
     </fieldset>
-  )
-})
+  );
+});
 
-export default PriceForm
+export default PriceForm;
--- components/steps/Step3.tsx
+++ components/steps/Step3.tsx
@@ -1,19 +1,18 @@
-'use client'
+"use client";
 
-import React from 'react'
-import PriceForm from '../categories/PriceForm'
-import AddressForm from '../categories/AddressForm'
-import PickupForm from '../categories/PickupForm'
+import React from "react";
+import PriceForm from "../categories/PriceForm";
+import AddressForm from "../categories/AddressForm";
+import PickupForm from "../categories/PickupForm";
 
 const Step3: React.FC = React.memo(() => {
   return (
-    <div className='step'>
-      <h2>Step 3</h2>
+    <div className="step flex flex-col gap-8">
       <PriceForm />
       <AddressForm />
       <PickupForm />
     </div>
-  )
-})
+  );
+});
 
-export default Step3
+export default Step3;
