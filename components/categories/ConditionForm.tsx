796d55d8117cfb984f43f4aa6414b7bf4e5da890
Julia <jsgoransson@gmail.com>
2024-09-27 02:10:49 +0200

    removed unused var..

--- components/categories/ConditionForm.tsx
+++ components/categories/ConditionForm.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { useFormContext } from "react-hook-form";
 import { ProductFormData } from "@/schemas";
 import {
-  TextField,
   Select,
   MenuItem,
   FormControl,
